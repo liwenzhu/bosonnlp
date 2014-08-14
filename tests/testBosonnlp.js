a44acb33e68c013e50a848b0ab4849da2a1bb59c
liwenzhu <lee.wenzhu@gmail.com>
2014-08-14 12:05:33 +0800

    ner sdk support single or multi documents.

--- tests/testBosonnlp.js
+++ tests/testBosonnlp.js
@@ -1,13 +1,35 @@
 'use strict';
 
 var bosonnlp = require('../index');
-var boson = new bosonnlp.BosonNlp("59G4ZvQp.2193.0YmDde8uiv3e");
+var boson = new bosonnlp.BosonNlp("YOUR_API_KEY");
 
-exports.testNER = function (test) {
-	boson.ner("地址在北京东四北大街", function (data) {
+exports.testNERSingle = function (test) {
+	var text = "成都商报记者 姚永忠";
+	boson.ner(text, function (data) {
+		data = JSON.parse(data)[0]; 
 		var entity = data.entity[0];
-		test.equal(data.word.slice(entity[0], entity[1]).join(''), "北京东四北大街");
-		test.equal(entity[2], "location");
+		test.equal(data.word.slice(entity[0], entity[1]).join(''), "成都商报");
+		test.equal(entity[2], "product_name");
+		test.done();
+	});
+};
+
+exports.testNERMulti = function (test) {
+	var text = ["对于该小孩是不是郑尚金的孩子，目前已做亲子鉴定，结果还没出来，", "纪检部门仍在调查之中。成都商报记者 姚永忠"];
+	boson.ner(text, function (data) {
+		data = JSON.parse(data);
+		var entity = data[0].entity[0];
+		test.equal(data[0].word.slice(entity[0], entity[1]).join(''), "郑尚金");
+		test.equal(entity[2], "person_name");
+		entity = data[1].entity[0]
+		test.equal(data[1].word.slice(entity[0], entity[1]).join(''), "纪检部门");
+		test.equal(entity[2], "org_name");
+		entity = data[1].entity[1]
+		test.equal(data[1].word.slice(entity[0], entity[1]).join(''), "成都商报");
+		test.equal(entity[2], "product_name");
+		entity = data[1].entity[2]
+		test.equal(data[1].word.slice(entity[0], entity[1]).join(''), "姚永忠");
+		test.equal(entity[2], "person_name");
 		test.done();
 	});
 };
